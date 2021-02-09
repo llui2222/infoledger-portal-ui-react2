53f8c42fe5be8fd5e34c38579ea2e401853813a3
Stanislav Ladutsko <x.stass@gmail.com>
2021-02-09 16:34:22 +0200

    IL-12: Fix eslint

--- craco.config.js
+++ craco.config.js
@@ -4,8 +4,4 @@ module.exports = {
             reporters: ["jest-sonar-reporter"]
         }
     },
-    eslint: {
-        "extends": ["plugin:testing-library/react", "plugin:jest-dom/recommended"],
-        "plugins": ["testing-library", "jest-dom"],
-    },
 };
