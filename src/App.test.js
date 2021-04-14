4d4e7736e56a087883adc5d41b55dd32b738f8ee
Stanislav Ladutsko <x.stass@gmail.com>
2021-04-14 20:36:15 +0300

    Remove router from App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,9 @@ import React from "react";
 import {render } from '@testing-library/react'
 import regeneratorRuntime from "regenerator-runtime";
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('<App />', () => {
   test('<App /> renders successfully', () => {
-    render(<Router><App /></Router>);
+    render(<App />);
   });
 });
