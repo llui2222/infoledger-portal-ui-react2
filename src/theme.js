39b5e030172c4753d9651573d5dca34c65e0cf7c
Stanislav Ladutsko <x.stass@gmail.com>
2021-05-05 17:11:37 +0300

    IL-80: Create theme for Dialog component

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,8 @@
 import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core';
 import { blue, green } from '@material-ui/core/colors';
 
+const spacing = multiplier => 8 * multiplier;
+
 const theme = createMuiTheme({
     palette: {
         primary: {
@@ -11,7 +13,28 @@ const theme = createMuiTheme({
             secondary: green[700],
         },
     },
-    spacing: 8
+    spacing: spacing(1),
+    overrides: {
+        MuiDialog: {
+            paper: {
+                overflowY: 'visible'
+            }
+        },
+        MuiDialogTitle: {
+            root: {
+                position: 'absolute',
+                top: -58,
+                paddingLeft: 2,
+                color: 'white'
+            }
+        },
+        MuiDialogContent: {
+            root: {
+                padding: `${spacing(2)}px ${spacing(6)}px`,
+                textAlign: 'center'
+            }
+        }
+    },
 });
 
 export default theme;
